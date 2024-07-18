1bd6f3e855ed90bea68d594045b7d1772341a905
View <mastertime7824@gmail.com>
2024-07-18 12:38:49 +0700

    make heartbeat

--- heart_beat_ver/heart_beat_ver.ino
+++ heart_beat_ver/heart_beat_ver.ino
@@ -1,8 +1,3 @@
-// Library
-// https://www.arduino.cc/reference/en/libraries/ethernet/
-// https://github.com/miguelbalboa/rfid
-// http://arduino.esp8266.com/stable/package_esp8266com_index.json
-//ASHA TECH CO
 #include <SPI.h>
 #include <MFRC522.h>
 #include <Ethernet.h>
@@ -19,13 +14,12 @@ static const uint8_t D8 = 15;
 static const uint8_t D9 = 3;
 static const uint8_t D10 = 1;
 
-//pin door
 #define LED_PIN D2
-//pinbuzzer
 #define BUZZER D0
 
 #define SS_PIN D4
 #define RST_PIN D3
+
 
 /*
 22:33:CA:76:5E:06  device 1
@@ -40,35 +34,33 @@ static const uint8_t D10 = 1;
 0x5B, 0xC2, 0x69, 0x23, 0xC0, 0x90
 
 */
-byte mac[] = {0x22, 0x33, 0xCA, 0x76, 0x5E, 0x06 };
-IPAddress ip(192, 168, 95, 1); //device 1,2,3
+byte mac[] = {0x5B, 0xC2, 0x69, 0x23, 0xC0, 0x90};
+IPAddress ip(192, 168, 95, 1);
 IPAddress dns(172, 17, 5, 227);
 IPAddress gateway(192, 168, 95, 254);
 IPAddress subnet(255, 255, 255, 0);
 String gate = "SS Gate 1";
 
-//ip api here
 IPAddress serverIP(172, 17, 5, 83);
 int serverPort = 3000;
 
 EthernetClient client;
-
 MFRC522 rfid(SS_PIN, RST_PIN);
 
+unsigned long previousMillis = 0;
+const long interval = 300000; // 60 seconds for heartbeat
+
+bool heartbeatPending = false;
+bool apiPending = false;
+String apiData = "";
+
 void setup() {
-  // Open serial communications and wait for port to open:
   Serial.begin(9600);
-  
-  while (!Serial) {
-    ;  // wait for serial port to connect. Needed for native USB port only
-  }
+  while (!Serial) { ; }
 
   pinMode(LED_PIN, OUTPUT);
   pinMode(BUZZER, OUTPUT);
   digitalWrite(BUZZER, LOW);
-  
-
-
 
   Ethernet.init(5);
   Ethernet.begin(mac, ip, dns, gateway, subnet);
@@ -76,47 +68,96 @@ void setup() {
   Serial.println();
   Serial.println(Ethernet.localIP());
 
-  SPI.begin();      // Init SPI bus
-  rfid.PCD_Init();  // Init MFRC522
-
+  SPI.begin();
+  rfid.PCD_Init();
 }
 
 void loop() {
-  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
-  if (!rfid.PICC_IsNewCardPresent())
-    return;
+  unsigned long currentMillis = millis();
 
-  // Verify if the NUID has been readed
-  if (!rfid.PICC_ReadCardSerial())
-    return;
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    heartbeatPending = true;
+  }
+
+  if (heartbeatPending) {
+    sendHeartbeat();
+  }
+
+  if (!rfid.PICC_IsNewCardPresent()) return;
+  if (!rfid.PICC_ReadCardSerial()) return;
 
   Serial.println(F("A new card has been detected."));
+  String rfid_in = rfid_read();
+  Serial.println(rfid_in);
+  apiData = rfid_in;
+  apiPending = true;
 
-  String rfid_in = rfid_read();  // Read RFID data
-  Serial.println(rfid_in);       // Print RFID data to serial monitor
-  sendToAPI(rfid_in);
-
-  // Halt PICC
   rfid.PICC_HaltA();
-
-  // Stop encryption on PCD
   rfid.PCD_StopCrypto1();
   digitalWrite(BUZZER, LOW);
+
+  if (apiPending) {
+    sendToAPI(apiData);
+  }
 }
 
-void sendToAPI(String sn) {
- 
-  String url = "/api/canteen/buffet";
- 
- //edit machine here
-  String body = "{\"sn\": \"" + sn + "\", \"machine\": \"" + gate +"\"}";
+void sendHeartbeat() {
+  String url = "/api/machine/detect";
+  String ipAddress = Ethernet.localIP().toString();
+  String macAddress = "";
 
-  Serial.println("Sending payload:");
+  for (byte i = 0; i < 6; i++) {
+    if (mac[i] < 0x10) {
+      macAddress += "0";
+    }
+    macAddress += String(mac[i], HEX);
+    if (i < 5) {
+      macAddress += ":";
+    }
+  }
+
+  String body = "{\"device\": \"" + gate + "\", \"ip\": \"" + ipAddress + "\", \"mac\": \"" + macAddress + "\"}";
+
+  // Print the data being sent
+  Serial.println("Sending Heartbeat Data:");
   Serial.println(body);
 
   if (client.connect(serverIP, serverPort)) {
-    Serial.println("Connected to server");
+    client.println("POST " + url + " HTTP/1.1");
+    client.println("Host: " + serverIP.toString());
+    client.println("Content-Type: application/json");
+    client.print("Content-Length: ");
+    client.println(body.length());
+    client.println();
+    client.println(body);
 
+    while (client.connected()) {
+      if (client.available()) {
+        String line = client.readStringUntil('\n');
+        if (line.startsWith("HTTP/1.1 ")) {
+          int statusCode = line.substring(9, 12).toInt();
+          if (statusCode == 200) {
+            Serial.println("Heartbeat sent successfully");
+            heartbeatPending = false;
+          }
+          break;
+        }
+      }
+    }
+    client.stop();
+  } else {
+    Serial.println("Heartbeat failed, device might be offline");
+    heartbeatPending = false;
+  }
+}
+
+
+void sendToAPI(String sn) {
+  String url = "/api/canteen/buffet";
+  String body = "{\"sn\": \"" + sn + "\", \"machine\": \"" + gate + "\"}";
+
+  if (client.connect(serverIP, serverPort)) {
     client.println("POST " + url + " HTTP/1.1");
     client.println("Host: " + serverIP.toString());
     client.println("Content-Type: application/json");
@@ -129,8 +170,6 @@ void sendToAPI(String sn) {
     while (client.connected()) {
       if (client.available()) {
         String line = client.readStringUntil('\n');
-        Serial.println(line);
-
         if (line.startsWith("HTTP/1.1 ")) {
           int statusCode = line.substring(9, 12).toInt();
           Serial.print("Response status code: ");
@@ -148,7 +187,6 @@ void sendToAPI(String sn) {
             digitalWrite(BUZZER, HIGH);
             delay(1000);
             digitalWrite(BUZZER, LOW);
-
           }
           break;
         }
@@ -161,6 +199,7 @@ void sendToAPI(String sn) {
 
     client.stop();
     Serial.println("Disconnected from server");
+    apiPending = false;
   } else {
     Serial.println("Connection failed");
     digitalWrite(BUZZER, HIGH);
@@ -174,21 +213,17 @@ void sendToAPI(String sn) {
     digitalWrite(BUZZER, HIGH);
     delay(300);
     digitalWrite(BUZZER, LOW);
+    apiPending = false;
   }
 }
 
 String rfid_read() {
   String content = "";
-
-  // Concatenate UID parts
   for (int i = rfid.uid.size - 1; i >= 0; i--) {
     content.concat(String(rfid.uid.uidByte[i] < 0x10 ? "0" : ""));
     content.concat(String(rfid.uid.uidByte[i], HEX));
   }
-
-  content.toUpperCase();  // Convert to uppercase
-
-  Serial.println(content);
+  content.toUpperCase();
 
   unsigned long decimalValue = strtoul(content.c_str(), NULL, 16);
   String decimalString = String(decimalValue);
@@ -197,5 +232,5 @@ String rfid_read() {
     decimalString = "0" + decimalString;
   }
 
-  return decimalString;  // Return RFID UID as String
+  return decimalString;
 }
